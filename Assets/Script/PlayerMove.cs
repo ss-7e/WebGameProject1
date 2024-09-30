b4b1d45f9fa4ac5592f3d9ea771f792d05434a2d
ss-7e <60181968+ss-7e@users.noreply.github.com>
2024-09-30 17:06:03 +0800

    playerMove

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 MoveValue;
+    public float MoveSpeed;
+
+    void OnMove(InputValue value)
     {
+        MoveValue = value.Get<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
